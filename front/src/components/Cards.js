c9ab0529735ee4cb4df84de72dbbf5ee6e38cfdd
Sandy Prolhac <tottati@hotmail.com>
2019-07-16 00:45:54 +0200

    install react-tiny-link

---
+++ front/src/components/Cards.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import ReactTinyLink from 'react-tiny-link';
+
+const Cards = () => (
+  <ReactTinyLink
+    cardSize="small"
+    showGraphic
+    maxLine={2}
+    minLine={1}
+    url="https://medium.com/better-programming/role-based-authorization-in-react-c70bb7641db4"
+  />
+);
+
+export default Cards;
