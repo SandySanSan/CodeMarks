b64206373c323de49d089ff9e65153420d0bc386
Sandy Prolhac <tottati@hotmail.com>
2019-07-16 12:28:22 +0200

    insert dans table de jointure ok

--- back/routes/content.js
+++ back/routes/content.js
@@ -3,19 +3,35 @@ const db = require('../connection');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const formData = req.body;
-  db.query('INSERT INTO content SET ?', formData, (err) => {
-    if (err) {
-      return res.status(500).json({
-        error: err.message,
-        sql: err.sql
-      });
-    }
-    return res.json(formData);
+  const { tags, ...contentData } = formData;
+  console.log(formData)
 
-  });
+  try {
+    const status = await db.queryAsync('INSERT INTO content SET ?', contentData);
+    const results = await db.queryAsync('SELECT idcontent FROM content WHERE idcontent = ?', status.insertId);
+    console.log(status)
+    const contentSelectId = results[0].idcontent;
+
+    const tagsValues = tags.map(tag => [contentSelectId, tag]);
+
+    const [tagValues] = await Promise.all([
+      db.queryAsync('INSERT INTO contentHasTag (contentId,tagId) VALUES ?', [tagsValues])
+    ]);
+    res.status(200).json({
+      tagValues
+    });
+    return contentData;
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message,
+      sql: err.sql
+    });
+  }
 });
+
+
 
 router.get('/', (req, res) => {
   db.query('SELECT * FROM content', (err) => {
